3ecf08c5ad8bd44d600618cb175cf6e3e0c020bd
s0ares999 <pedro_mdsoares@hotmail.com>
2025-01-22 23:14:06 +0000

    Update consultaratleta_screen.dart

--- cliente/lib/presentation/screens/consultaratleta_screen.dart
+++ cliente/lib/presentation/screens/consultaratleta_screen.dart
@@ -20,8 +20,8 @@ class _ConsultarAtletaScreenState extends State<ConsultarAtletaScreen> {
   @override
   void initState() {
     super.initState();
-    _fetchAthletes(); // Busca os atletas
-    _fetchReports(); // Busca os relatórios
+    _fetchAthletes();
+    _fetchReports();
   }
 
   Future<void> _fetchAthletes() async {
@@ -30,20 +30,15 @@ class _ConsultarAtletaScreenState extends State<ConsultarAtletaScreen> {
       final response = await http.get(Uri.parse(url));
 
       if (response.statusCode == 200) {
-        // Decodificar a resposta do servidor
         final List<dynamic> decodedData = json.decode(response.body);
-
-        // Atualizar o estado com os dados
         setState(() {
           athletes = List<Map<String, dynamic>>.from(decodedData);
         });
       } else {
-        // Exibir uma mensagem de erro caso a resposta não seja 200
         _showErrorDialog(
             'Erro ao carregar atletas. Código: ${response.statusCode}');
       }
     } catch (e) {
-      // Tratar erros de conexão ou outros problemas
       _showErrorDialog('Erro de conexão: ${e.toString()}');
     }
   }
@@ -54,20 +49,15 @@ class _ConsultarAtletaScreenState extends State<ConsultarAtletaScreen> {
       final response = await http.get(Uri.parse(url));
 
       if (response.statusCode == 200) {
-        // Decodificar a resposta do servidor
         final List<dynamic> decodedData = json.decode(response.body);
-
-        // Atualizar o estado com os relatórios
         setState(() {
           reports = List<Map<String, dynamic>>.from(decodedData);
         });
       } else {
-        // Exibir uma mensagem de erro caso a resposta não seja 200
         _showErrorDialog(
             'Erro ao carregar relatórios. Código: ${response.statusCode}');
       }
     } catch (e) {
-      // Tratar erros de conexão ou outros problemas
       _showErrorDialog('Erro de conexão: ${e.toString()}');
     }
   }
@@ -90,127 +80,11 @@ class _ConsultarAtletaScreenState extends State<ConsultarAtletaScreen> {
     );
   }
 
-  void _sortAthletes(String criterion) {
-    setState(() {
-      if (criterion == 'Ano') {
-        athletes.sort((a, b) =>
-            (a['data_nascimento'] ?? '').compareTo(b['data_nascimento'] ?? ''));
-      } else if (criterion == 'Clube') {
-        athletes.sort((a, b) => (a['clube'] ?? '').compareTo(b['clube'] ?? ''));
-      }
-    });
-  }
-
-  List<Map<String, dynamic>> _filterByRating() {
-    return athletes.where((athlete) {
-      // Filtra os atletas conforme o rating selecionado
-      return athlete['rating'] == selectedRating;
-    }).toList();
-  }
-
-  Widget _buildPositionFilter() {
-    final positions = [
-      'Guarda-Redes',
-      'Defesa Central',
-      'Defesa Esquerda',
-      'Defesa Direita',
-      'Médio',
-      'Atacante'
-    ];
-
-    return Column(
-      children: [
-        GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: 10.0,
-            mainAxisSpacing: 10.0,
-            childAspectRatio:
-                1.5, // Ajuste a proporção largura/altura dos cards
-          ),
-          itemCount: positions.length,
-          shrinkWrap: true,
-          physics: const NeverScrollableScrollPhysics(),
-          itemBuilder: (BuildContext context, int index) {
-            return ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  selectedPosition = selectedPosition == positions[index]
-                      ? ''
-                      : positions[index];
-                });
-              },
-              style: ElevatedButton.styleFrom(
-                padding:
-                    const EdgeInsets.all(8), // Adiciona mais espaço interno
-                backgroundColor: selectedPosition == positions[index]
-                    ? Colors.orange
-                    : Colors.grey[300],
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-              child: FittedBox(
-                fit: BoxFit
-                    .scaleDown, // Ajusta automaticamente o texto ao espaço
-                child: Text(
-                  positions[index],
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: selectedPosition == positions[index]
-                        ? Colors.white
-                        : Colors.black,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 14, // Pode ajustar o tamanho da fonte
-                  ),
-                ),
-              ),
-            );
-          },
-        ),
-        const SizedBox(height: 10),
-        ElevatedButton(
-          onPressed: () => Navigator.of(context).pop(),
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.orange,
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-          ),
-          child: const Text(
-            'Aplicar',
-            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget _buildRatingFilter() {
-    return Column(
-      children: List.generate(5, (index) {
-        return RadioListTile<int>(
-          title: Text('${index + 1} Estrela'),
-          value: index + 1,
-          groupValue: selectedRating,
-          onChanged: (int? value) {
-            setState(() {
-              selectedRating = value!;
-            });
-            Navigator.of(context).pop();
-          },
-        );
-      }),
-    );
-  }
-
-  // Função para obter o ratingFinalSelecionado de um atleta
   int _getAthleteRating(String athleteId) {
-    // Encontrar o relatório correspondente ao atleta
     final report = reports.firstWhere(
       (report) => report['atleta_id'] == athleteId,
       orElse: () => {},
     );
-    // Retornar o ratingFinalSelecionado do relatório, ou 0 caso não exista
     return report.isNotEmpty ? report['ratingFinalSelecionado'] ?? 0 : 0;
   }
 
@@ -218,9 +92,10 @@ class _ConsultarAtletaScreenState extends State<ConsultarAtletaScreen> {
   Widget build(BuildContext context) {
     List<Map<String, dynamic>> filteredAthletes = athletes;
 
-    // Aplica o filtro de rating se o filtro de rating estiver ativo
     if (selectedRating != 3) {
-      filteredAthletes = _filterByRating();
+      filteredAthletes = athletes.where((athlete) {
+        return athlete['rating'] == selectedRating;
+      }).toList();
     }
 
     return Scaffold(
@@ -252,29 +127,6 @@ class _ConsultarAtletaScreenState extends State<ConsultarAtletaScreen> {
                     setState(() {
                       selectedFilter = index;
                     });
-                    if (filters[index] == 'Ano' || filters[index] == 'Clube') {
-                      _sortAthletes(filters[index]);
-                    } else if (filters[index] == 'Posição') {
-                      showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return AlertDialog(
-                            title: const Text('Filtrar por Posição'),
-                            content: _buildPositionFilter(),
-                          );
-                        },
-                      );
-                    } else if (filters[index] == 'Rating') {
-                      showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return AlertDialog(
-                            title: const Text('Filtrar por Rating'),
-                            content: _buildRatingFilter(),
-                          );
-                        },
-                      );
-                    }
                   },
                   child: Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 16.0),
@@ -314,24 +166,19 @@ class _ConsultarAtletaScreenState extends State<ConsultarAtletaScreen> {
 
                 return Container(
                   margin: const EdgeInsets.symmetric(
-                      vertical: 6.0,
-                      horizontal:
-                          16.0), // Margem para dar espaço entre os itens
+                      vertical: 6.0, horizontal: 16.0),
                   decoration: BoxDecoration(
-                    color: Colors.white, // Cor de fundo do item
-                    borderRadius:
-                        BorderRadius.circular(10), // Borda arredondada
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
                     boxShadow: [
                       BoxShadow(
-                        color: Colors.grey
-                            .withOpacity(0.3), // Cor e opacidade da sombra
-                        spreadRadius: 2, // Quanto a sombra se espalha
-                        blurRadius: 5, // Intensidade do desfoque
-                        offset: Offset(0, 2), // Posição da sombra
+                        color: Colors.grey.withOpacity(0.3),
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                        offset: Offset(0, 2),
                       ),
                     ],
                   ),
-
                   child: ListTile(
                     title: Text(athlete['nome']),
                     subtitle: Text(
@@ -350,12 +197,93 @@ class _ConsultarAtletaScreenState extends State<ConsultarAtletaScreen> {
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(10),
                     ),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => DetalhesAtletaScreen(
+                            athlete: athlete, reports: [],
+                          ),
+                        ),
+                      );
+                    },
                   ),
                 );
               },
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class DetalhesAtletaScreen extends StatelessWidget {
+  final Map<String, dynamic> athlete;
+  final List<Map<String, dynamic>> reports;
+
+  const DetalhesAtletaScreen({
+    required this.athlete,
+    required this.reports,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    // Filtra os relatórios que pertencem ao atleta
+    final athleteReports = reports.where((report) {
+      return report['atleta_id'] == athlete['id'];
+    }).toList();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(athlete['nome']),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Nome: ${athlete['nome']}', style: const TextStyle(fontSize: 18)),
+              const SizedBox(height: 10),
+              Text('Clube: ${athlete['clube']}', style: const TextStyle(fontSize: 16)),
+              const SizedBox(height: 10),
+              Text('Posição: ${athlete['posicao']}', style: const TextStyle(fontSize: 16)),
+              const SizedBox(height: 10),
+              Text('Ano: ${athlete['ano']}', style: const TextStyle(fontSize: 16)),
+              const SizedBox(height: 20),
+              Text('Relatórios:', style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+              const SizedBox(height: 10),
+              if (athleteReports.isEmpty)
+                const Text('Nenhum relatório disponível para este atleta.', style: TextStyle(fontSize: 16)),
+              if (athleteReports.isNotEmpty)
+                Column(
+                  children: athleteReports.map((report) {
+                    return Card(
+                      elevation: 4,
+                      margin: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Título: ${report['titulo']}', style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                            const SizedBox(height: 5),
+                            Text('Data: ${report['data']}', style: const TextStyle(fontSize: 14)),
+                            const SizedBox(height: 5),
+                            Text('Observações: ${report['observacoes']}', style: const TextStyle(fontSize: 14)),
+                            const SizedBox(height: 5),
+                            Text('Rating: ${report['ratingFinalSelecionado']}', style: const TextStyle(fontSize: 14)),
+                          ],
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                ),
+            ],
+          ),
+        ),
       ),
     );
   }
